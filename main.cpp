16ac5089d1d17bfaa522875015f44396022a2564
Ha Hoang Kim Chi <hahoangkimchi1608@gmail.com>
2025-05-08 07:29:38 +0700

    thêm nút back

--- main.cpp
+++ main.cpp
@@ -54,26 +54,23 @@ void playMusic(Mix_Music* gMusic) {
 }
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
-        return 1;
-    }
 
     int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
 
     if (TTF_Init() == -1) {
+        cerr << "SDL_ttf could not initialize! SDL_ttf Error: " << TTF_GetError() << endl;
         IMG_Quit();
         SDL_Quit();
         return 1;
     }
 
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        cerr << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << endl;
         TTF_Quit();
         IMG_Quit();
         SDL_Quit();
         return 1;
     }
-
-    int mixFlags = MIX_INIT_MP3;
 
     SDL_Window* window = SDL_CreateWindow("Gold Miner - Swinging Rope", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
@@ -82,9 +79,9 @@ int main(int argc, char* argv[]) {
     // Load Font
     TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
 
-        SDL_Texture* ropeSegmentTexture = nullptr;
+    SDL_Texture* ropeSegmentTexture = nullptr;
     {
-        SDL_Surface* surf = SDL_CreateRGBSurface(0, ROPE_THICKNESS, 1, 32, 0, 0, 0, 0); // Width = thickness, Height = 1
+        SDL_Surface* surf = SDL_CreateRGBSurface(0, ROPE_THICKNESS, 1, 32, 0, 0, 0, 0);
         if (!surf) {
              cerr << "Failed to create rope surface! SDL Error: " << SDL_GetError() << endl;
              if(font) TTF_CloseFont(font);
@@ -93,7 +90,7 @@ int main(int argc, char* argv[]) {
              Mix_CloseAudio(); Mix_Quit(); TTF_Quit(); IMG_Quit(); SDL_Quit();
              return 1;
         } else {
-            SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 255, 255, 255)); // Fill white (color doesn't matter much if modulated)
+            SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 255, 255, 255));
             ropeSegmentTexture = SDL_CreateTextureFromSurface(renderer, surf);
             if (!ropeSegmentTexture) {
                  cerr << "Failed to create rope texture! SDL Error: " << SDL_GetError() << endl;
@@ -122,6 +119,7 @@ int main(int argc, char* argv[]) {
     SDL_Texture* noButtonTexture = loadTexture(renderer, "no.png");
     SDL_Texture* instructionsButtonTexture = loadTexture(renderer, "instructions.png");
     SDL_Texture* introTexture = loadTexture(renderer, "intro.jpg");
+    SDL_Texture* backButtonTexture = loadTexture(renderer, "back.png"); // <<< ADDED: Load back button texture
     Mix_Music* backgroundMusic = loadMusic("background_music.mp3");
 
     int playerWidth = 76;
@@ -149,10 +147,11 @@ int main(int argc, char* argv[]) {
 
     srand(time(0));
 
-    SDL_Rect startButtonRect = {20, 100, 300, 250}; // x,y,w,h
+    SDL_Rect startButtonRect = {20, 100, 300, 250};
     SDL_Rect instructionsButtonRect = {20, 320, 230, 100};
     SDL_Rect yesButtonRect = {70, SCREEN_HEIGHT / 2 + 50, 200, 70};
     SDL_Rect noButtonRect = {500, SCREEN_HEIGHT / 2 + 50, 200, 70};
+    SDL_Rect backButtonRect = {700, 40, 60, 40};
 
     auto resetGame = [&]() {
         golds.clear();
@@ -165,7 +164,6 @@ int main(int argc, char* argv[]) {
         rope = Rope(ropeSegmentTexture, ROPE_THICKNESS, ropeStartX, ropeStartY, ropeMaxLength);
         totalGoldValue = 0;
 
-        //tao vang va da
         for (int i = 0; i < 10; ++i) {
             Gold newGold = Gold::createRandomGold(renderer);
             newGold.texture = goldTexture;
@@ -178,7 +176,7 @@ int main(int argc, char* argv[]) {
             stones.push_back(newStone);
         }
 
-        targetScore = max(100, static_cast<int>(round(totalGoldValue * 2 / 3)));
+        targetScore = max(100, static_cast<int>(round(totalGoldValue * 2.0 / 3.0))); // Use 2.0/3.0 for float division
 
         gameState = PLAYING;
     };
@@ -198,7 +196,7 @@ int main(int argc, char* argv[]) {
                         SDL_Point mousePoint = {mouseX, mouseY};
 
                         if (SDL_PointInRect(&mousePoint, &startButtonRect)) {
-                            resetGame(); // Start a new game
+                            resetGame();
                         } else if (SDL_PointInRect(&mousePoint, &instructionsButtonRect)) {
                             gameState = INSTRUCTIONS;
                         }
@@ -206,11 +204,21 @@ int main(int argc, char* argv[]) {
                     break;
 
                 case PLAYING:
-                    if (e.type == SDL_KEYDOWN) {
+                    if (e.type == SDL_MOUSEBUTTONDOWN) {
+                        int mouseX, mouseY;
+                        SDL_GetMouseState(&mouseX, &mouseY);
+                        SDL_Point mousePoint = {mouseX, mouseY};
+
+                        if (SDL_PointInRect(&mousePoint, &backButtonRect)) {
+                            gameState = MENU;
+                            if (Mix_PlayingMusic()) { // Pause music when going to menu
+                                Mix_PauseMusic();
+                            }
+                        }
+                    }
+                    else if (e.type == SDL_KEYDOWN) {
                         if (e.key.keysym.sym == SDLK_SPACE) {
                             rope.extend();
-                        }
-                        else if (e.key.keysym.sym == SDLK_ESCAPE) {
                         }
                     }
                     break;
@@ -246,6 +254,7 @@ int main(int argc, char* argv[]) {
             }
         }
         if (gameState == PLAYING) {
+            playMusic(backgroundMusic); // Ensure music plays/resumes when entering PLAYING state
             if (!gameOver) {
                 Uint32 elapsedTime = SDL_GetTicks() - gameStartTime;
                 remainingSeconds = max(0, gameDurationSeconds - static_cast<int>(elapsedTime / 1000));
@@ -257,14 +266,17 @@ int main(int argc, char* argv[]) {
             if (!gameOver) {
                  rope.update(score, golds, stones);
             }
+        } else if (gameState == MENU) { // Added this to ensure music plays on menu
+            playMusic(backgroundMusic);
         }
+
 
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderClear(renderer);
 
         switch (gameState) {
             case MENU:
-                playMusic(backgroundMusic);
+                // playMusic(backgroundMusic); // Moved up to playMusic always when in MENU state
                 if (menuTexture) SDL_RenderCopy(renderer, menuTexture, NULL, NULL);
                 if (startButtonTexture) SDL_RenderCopy(renderer, startButtonTexture, NULL, &startButtonRect);
                 if (instructionsButtonTexture) SDL_RenderCopy(renderer, instructionsButtonTexture, NULL, &instructionsButtonRect);
@@ -279,35 +291,35 @@ int main(int argc, char* argv[]) {
                 rope.render(renderer);
                 player.render(renderer);
 
+                if (backButtonTexture) SDL_RenderCopy(renderer, backButtonTexture, NULL, &backButtonRect); // <<< ADDED: Render back button
+
                 if (font) {
                     SDL_Color textColor = {255, 255, 255, 255};
                     string scoreText = "$" + to_string(score);
                     string timeText = to_string(remainingSeconds);
                     string targetText = " $" + to_string(targetScore);
 
-                    SDL_Surface* scoreSurface = TTF_RenderText_Blended(font, scoreText.c_str(), textColor); // Use Blended for better quality
+                    SDL_Surface* scoreSurface = TTF_RenderText_Blended(font, scoreText.c_str(), textColor);
                     SDL_Surface* timerSurface = TTF_RenderText_Blended(font, timeText.c_str(), textColor);
                     SDL_Surface* targetSurface = TTF_RenderText_Blended(font, targetText.c_str(), textColor);
 
                     if (scoreSurface) {
-                        SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
+                        SDL_Texture* scoreTextureUI = SDL_CreateTextureFromSurface(renderer, scoreSurface); // Renamed to avoid conflict
                         SDL_Rect scoreRect = {85, 12, scoreSurface->w, scoreSurface->h};
-                        SDL_RenderCopy(renderer, scoreTexture, nullptr, &scoreRect);
-                        SDL_DestroyTexture(scoreTexture); SDL_FreeSurface(scoreSurface);
+                        SDL_RenderCopy(renderer, scoreTextureUI, nullptr, &scoreRect);
+                        SDL_DestroyTexture(scoreTextureUI); SDL_FreeSurface(scoreSurface);
                     }
                     if (timerSurface) {
                         SDL_Texture* timerTexture = SDL_CreateTextureFromSurface(renderer, timerSurface);
-                         // Position Timer top-right
                         SDL_Rect timerRect = {SCREEN_WIDTH - timerSurface->w - 20, 10, timerSurface->w, timerSurface->h};
                         SDL_RenderCopy(renderer, timerTexture, nullptr, &timerRect);
                         SDL_DestroyTexture(timerTexture); SDL_FreeSurface(timerSurface);
                     }
                     if (targetSurface) {
-                        SDL_Texture* targetTexture = SDL_CreateTextureFromSurface(renderer, targetSurface);
-                         // Position Target below Score
+                        SDL_Texture* targetTextureUI = SDL_CreateTextureFromSurface(renderer, targetSurface); // Renamed
                         SDL_Rect targetRect = {80, 50, targetSurface->w, targetSurface->h};
-                        SDL_RenderCopy(renderer, targetTexture, nullptr, &targetRect);
-                        SDL_DestroyTexture(targetTexture); SDL_FreeSurface(targetSurface);
+                        SDL_RenderCopy(renderer, targetTextureUI, nullptr, &targetRect);
+                        SDL_DestroyTexture(targetTextureUI); SDL_FreeSurface(targetSurface);
                     }
                 }
                 break;
@@ -327,20 +339,49 @@ int main(int argc, char* argv[]) {
             case INSTRUCTIONS:
                 if (introTexture) SDL_RenderCopy(renderer, introTexture, NULL, NULL);
                 if (font) {
-                    SDL_Color textColor = {255, 255, 255, 255};
-                    const char* line1 = " ";
+                    SDL_Color textColor = {255, 255, 255, 255}; // White text
+                    SDL_Color shadowColor = {0, 0, 0, 255};     // Black shadow
 
-                    SDL_Surface* surf1 = TTF_RenderText_Blended(font, line1, textColor);
+                    const char* lines[] = {
+                        " "
+                    };
+                    int num_lines = sizeof(lines) / sizeof(lines[0]);
+                    int yPos = 60; // Initial Y position
 
-                    int yPos = SCREEN_HEIGHT / 2 - 60;
-                    if(surf1) { SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surf1); SDL_Rect r = {SCREEN_WIDTH/2 - surf1->w/2, yPos, surf1->w, surf1->h}; SDL_RenderCopy(renderer, tex, NULL, &r); SDL_DestroyTexture(tex); SDL_FreeSurface(surf1); yPos += r.h + 5; }
+                    for (int i = 0; i < num_lines; ++i) {
+                        if (strlen(lines[i]) > 0) { // Only render non-empty lines
+                            // Render shadow first
+                            SDL_Surface* surfShadow = TTF_RenderText_Blended(font, lines[i], shadowColor);
+                            if (surfShadow) {
+                                SDL_Texture* texShadow = SDL_CreateTextureFromSurface(renderer, surfShadow);
+                                SDL_Rect rShadow = {SCREEN_WIDTH / 2 - surfShadow->w / 2 + 1, yPos + 1, surfShadow->w, surfShadow->h}; // Offset for shadow
+                                SDL_RenderCopy(renderer, texShadow, NULL, &rShadow);
+                                SDL_DestroyTexture(texShadow);
+                                SDL_FreeSurface(surfShadow);
+                            }
 
+                            // Render actual text
+                            SDL_Surface* surfText = TTF_RenderText_Blended(font, lines[i], textColor);
+                            if (surfText) {
+                                SDL_Texture* texText = SDL_CreateTextureFromSurface(renderer, surfText);
+                                SDL_Rect rText = {SCREEN_WIDTH / 2 - surfText->w / 2, yPos, surfText->w, surfText->h};
+                                SDL_RenderCopy(renderer, texText, NULL, &rText);
+                                SDL_DestroyTexture(texText);
+                                SDL_FreeSurface(surfText);
+                                yPos += rText.h + 5; // Move Y position for next line
+                            } else {
+                                yPos += TTF_FontHeight(font) + 5; // Fallback line height
+                            }
+                        } else {
+                             yPos += (TTF_FontHeight(font) / 2) + 5; // Smaller gap for empty lines
+                        }
+                    }
                 }
                 break;
         }
 
         SDL_RenderPresent(renderer);
-
+        SDL_Delay(16); // Cap framerate roughly to 60 FPS
     }
 
     SDL_DestroyTexture(playerTexture);
@@ -355,6 +396,9 @@ int main(int argc, char* argv[]) {
     SDL_DestroyTexture(noButtonTexture);
     SDL_DestroyTexture(instructionsButtonTexture);
     SDL_DestroyTexture(introTexture);
+    SDL_DestroyTexture(backButtonTexture); // <<< ADDED: Destroy back button texture
+    SDL_DestroyTexture(ropeSegmentTexture);
+
 
     if (backgroundMusic) Mix_FreeMusic(backgroundMusic);
     if (font) TTF_CloseFont(font);
